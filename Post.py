558e809c0a740b7ca80a7578921a3ab958dc7a3e
KAllYANI2003 <cloudbook744@gmail.com>
2025-09-05 15:37:59 +0530

    next one

---
+++ Post.py
@@ -0,0 +1,101 @@
+from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, AnyUrl, EmailStr, computed_field
+from typing import Annotated, Optional, Dict, List, Literal
+import json
+
+# Literal is used to provide options to the user, Like can be used in gender selection
+
+app = FastAPI()
+
+class Patients(BaseModel):
+    id : Annotated[str, Field(..., description='id of the patient', example='P001')]
+    name : Annotated[str, Field(...,description='name of the patient', example='Kishan')]
+    city : Annotated[str, Field(..., description='name of city the patient belongs to')]
+    age : Annotated[int, Field(..., gt=0, lt=120)]
+    gender : Annotated[Literal['Male','Female','Others'], Field(..., description='Gender')]
+    height : Annotated[float, Field(..., le=7, ge=1, description='Height of the patient in feet')]
+    weight : Annotated[float, Field(..., gt=0, description='weight of patient')]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2) ,2)
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) -> str:
+        if (self.bmi < 18.5):
+            return "Underweight"
+        elif(self.bmi < 30):
+            return "Normal"
+        elif(self.bmi > 30):
+            return "Overweight"
+        
+def load_data():
+    with open('datas.json') as f:
+        data = json.load(f)
+        return data
+    
+def save_data(data):
+    with open('datas.json', 'w') as f:
+        json.dump(data, f)
+    
+
+@app.get("/")
+def hello():
+    return {"message": "Hello world"}
+
+@app.get("/about")
+def about():
+    return {"message": "Kishan is making a new web page that is hosted in this machine only"}
+
+@app.get("/contact")
+def kishan():
+    return {"message" : "Contact information : 9863139834 "}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
+
+@app.get("/patient/{pat_ID}")
+def patient(pat_ID:str = Path(..., description='ID of the patient', example='1')):
+    id = "P00" + (pat_ID)
+    data = load_data()
+    
+    if (id in data):
+        return data[id]
+    raise HTTPException(status_code=404, detail='Patient not found')
+
+@app.get("/sort")
+def sort_params(sort_by:str=Query(..., description="sort on the basis of given param"), 
+                ordr:str=Query(..., description="order of sorting")):
+    sort = ["height", "weight", "age"]
+    order = ['inc', 'dec']
+    if (sort_by not in sort):
+        raise HTTPException(status_code=400, detail=f'chose from {sort}')
+    if (ordr not in order):
+        raise HTTPException(status_code=400, detail=f'chose from {ordr}')
+    
+    data = load_data()  
+    sort_order = True if ordr=='dec' else False
+    
+    sorteddata = sorted(data.values(), key=lambda x:x.get(sort_by, 0), reverse=sort_order)
+    return sorteddata
+
+@app.post("/create")
+def create_patient(patient : Patients):
+    # Load all the existing data
+    data = load_data()
+    # Check if the given patient already exists or not
+    if (patient.id in data):
+        raise HTTPException(status_code=400, detail='Patient already exists !')
+    # Add to the database
+    data[patient.id] = patient.model_dump(exclude=['id'])
+    
+    # Save the data
+    save_data(data)
+    
+    return JSONResponse(status_code=201, content={'message':'patient created successfully'})
